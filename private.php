794f6f7e8551ee2efcf9fabf2df106b2b6dc1dc4
Matthias Nötzli <matthias.noetzli@gmail.com>
2021-11-15 08:21:11 +0100

    Template for private book login with redirect

---
+++ private.php
@@ -0,0 +1,33 @@
+<?php $bloginfourl = get_bloginfo( 'url' ); ?>
+
+	<div <?php post_class(); ?>>
+		<h2 class="entry-title denied-title"><?php _e( 'Access Denied', 'pressbooks-book' ); ?></h2>
+		<div class="entry-content denied-text">
+			<p>
+			<?php
+			printf(
+				/* translators: link to sign in */
+				__( 'This book is private, and accessible only to registered users. If you have an account you can %s.', 'pressbooks-book' ),
+				sprintf(
+					'<a href="%1$s">%2$s</a>',
+					esc_url( wp_login_url( get_the_permalink() ) ),
+					__( 'sign in here', 'pressbooks-book' )
+				)
+			);
+			?>
+			</p>
+			<p>
+			<?php
+			printf(
+				/* translators: link to Pressbooks.com */
+				__( 'You can also set up your own Pressbooks book at %s.', 'pressbooks-book' ),
+				sprintf(
+					'<a href="%1$s">%2$s</a>',
+					apply_filters( 'pb_signup_url', 'https://pressbooks.com' ),
+					apply_filters( 'pb_signup_title', 'Pressbooks.com' )
+				)
+			);
+			?>
+			</p>
+		</div>
+	</div><!-- #post-## -->
