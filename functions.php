2e1c68cc6a5622522261238cd9eff7480d53ce7e
Matthias Nötzli <91967281+matthias-noetzli@users.noreply.github.com>
2021-11-30 14:48:56 +0100

    Textbox Default Colors

--- functions.php
+++ functions.php
@@ -28,3 +28,35 @@ function hfh_pressbooks_theme_enqueue_scripts() {
 }
 
 add_action( 'wp_enqueue_scripts', 'hfh_pressbooks_theme_enqueue_scripts', 11 );
+
+
+/**
+ * Changes default colors for the different textboxes
+ * 
+ * @param array $default_options The default theme options.
+ */
+function hfh_pressbooks_theme_options_global_defaults( $default_options ) {
+	return array_merge(
+		$default_options,
+		array(
+			'edu_textbox_examples_header_color'        => '#FFF',
+			'edu_textbox_examples_header_background'   => '#005999',
+			'edu_textbox_examples_background'          => '#dbeffd',
+			'edu_textbox_exercises_header_color'       => '#FFF',
+			'edu_textbox_exercises_header_background'  => '#BE1925',
+			'edu_textbox_exercises_background'         => '#F8E8E9',
+			'edu_textbox_objectives_header_color'      => '#FFF',
+			'edu_textbox_objectives_header_background' => '#306100',
+			'edu_textbox_objectives_background'        => '#e2edc7',
+			'edu_textbox_takeaways_header_color'       => '#FFF',
+			'edu_textbox_takeaways_header_background'  => '#5F37AE',
+			'edu_textbox_takeaways_background'         => '#f1ebff',
+		)
+	);
+}
+
+add_filter(
+	'pb_theme_options_global_defaults',
+	'hfh_pressbooks_theme_options_global_defaults',
+	11
+);
