0f931f5824230c17c20efa6e2e3f67820c21d755
Matthias Nötzli <91967281+matthias-noetzli@users.noreply.github.com>
2022-03-28 14:31:57 +0200

    Update content-search.php

--- partials/content-search.php
+++ partials/content-search.php
@@ -3,14 +3,16 @@
     <a href="<?php echo esc_url(get_permalink()) ?>" class="entry-summary" rel="bookmark">
         <?php
         $keyword = get_search_query();
-        $string = wp_strip_all_tags(strip_shortcodes(get_the_content()));
-        if (preg_match("/^.*" . preg_quote($keyword, '/') . ".*$/mi", $string, $matches)) {
-            $result = $matches[0];
-            $regex = "/(" . preg_quote($keyword, '/') . ")/i";
-            echo sprintf('<div>%s</div>', preg_replace($regex, "<span class='hfh-search-term'>$1</span>", $result));
-        } else {
-            echo sprintf('<div>%s</a>', get_the_excerpt(get_the_ID()));
+        $output = sprintf('<div>%s</a>', get_the_excerpt(get_the_ID()));
+        if (!empty($keyword)) {
+            $string = wp_strip_all_tags(strip_shortcodes(get_the_content()));
+            if (preg_match("/^.*" . preg_quote($keyword, '/') . ".*$/imu", $string, $matches)) {
+                $result = $matches[0];
+                $regex = "/(" . preg_quote($keyword, '/') . ")/iu";
+                $output = sprintf('<div>%s</div>', preg_replace($regex, "<span class='hfh-search-term'>$1</span>", $result));
+            }
         }
+        echo $output;
         ?>
     </a><!-- .entry-summary -->
 </div><!-- #post-## -->
