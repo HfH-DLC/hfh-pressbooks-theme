784275f24d973e373d3b9be6f0e61243e19d91ac
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-04-06 15:34:40 +0200

    Change search result to hfh-teaser

--- partials/content-search.php
+++ partials/content-search.php
@@ -1,18 +1,31 @@
-<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <?php the_title(sprintf('<h2 class="section-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
-    <a href="<?php echo esc_url(get_permalink()) ?>" class="entry-summary" rel="bookmark">
-        <?php
-        $keyword = get_search_query();
-        $output = sprintf('<div>%s</a>', get_the_excerpt(get_the_ID()));
-        if (!empty($keyword)) {
-            $string = wp_strip_all_tags(strip_shortcodes(get_the_content()));
-            if (preg_match("/^.*" . preg_quote($keyword, '/') . ".*$/imu", $string, $matches)) {
-                $result = $matches[0];
-                $regex = "/(" . preg_quote($keyword, '/') . ")/iu";
-                $output = sprintf('<div>%s</div>', preg_replace($regex, "<span class='hfh-search-term'>$1</span>", $result));
+<a href="<?php echo esc_url(get_permalink()) ?>" class="hfh-teaser">
+    <?php if (has_post_thumbnail()) : ?>
+        <div class="hfh-teaser__image">
+            <img src="<?= esc_url(the_post_thumbnail_url()) ?>" alt="<?= get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true); ?>" />
+        </div>
+    <?php endif; ?>
+    <div class="hfh-teaser__text-container">
+        <div class="hfh-teaser__title">
+            <?= the_title() ?>
+        </div>
+        <div class="hfh-teaser__text">
+            <?php
+            $keyword = get_search_query();
+            $output = sprintf('<div>%s</div>', get_the_excerpt(get_the_ID()));
+            if (!empty($keyword)) {
+                $string = wp_strip_all_tags(strip_shortcodes(get_the_content()));
+                if (preg_match("/^.*" . preg_quote($keyword, '/') . ".*$/imu", $string, $matches)) {
+                    $result = $matches[0];
+                    $regex = "/(" . preg_quote($keyword, '/') . ")/iu";
+                    $output = sprintf('<div>%s</div>', preg_replace($regex, "<span class='hfh-search-term'>$1</span>", $result));
+                }
             }
-        }
-        echo $output;
-        ?>
-    </a><!-- .entry-summary -->
-</div><!-- #post-## -->
+            echo $output;
+            ?>
+        </div>
+    </div>
+    <svg class="hfh-icon hfh-icon--arrow hfh-teaser__arrow" width="47" height="20" viewBox="0 0 47 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M45 10H0" stroke-width="2" stroke="currentColor" />
+        <path d="M36.5 1L45 10L36.5 19" stroke-width="2" stroke="currentColor" />
+    </svg>
+</a>
