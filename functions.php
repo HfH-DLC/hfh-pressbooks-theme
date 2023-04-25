ef6d85b2233780c9fb324f5202d71c98ebe6fd76
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-04-25 10:27:17 +0200

    Version 1.0.15

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if (!defined('HFH_PRESSBOOKS_THEME_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define('HFH_PRESSBOOKS_THEME_VERSION', '1.0.14');
+	define('HFH_PRESSBOOKS_THEME_VERSION', '1.0.15');
 }
 
 require_once 'inc/class-book.php';
